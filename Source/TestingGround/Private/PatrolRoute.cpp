ca8e6ca17deb88f1929a89614f5455e6893d50a6
dnguyen7433 <nguyendieuhuyen2809@gmail.com>
2019-03-17 12:37:44 -0500

    TG_Using PatrolRoute Component instead of PatrolRoute Inheritance

---
+++ Source/TestingGround/Private/PatrolRoute.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:239642ddb887cea9be5671cb996307738e68ff42f6869ca5fd3152d6fbd4c241
+size 195
---
+++ Source/TestingGround/Public/PatrolRoute.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0cac95d9cbdc80560ad78ef0641beac1b42707aa69fa98b2da608bbc1491ecdf
+size 566
