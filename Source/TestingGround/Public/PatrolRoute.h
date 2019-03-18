95be642cc17053168db6a445e58b9a80681f85c9
dnguyen7433 <nguyendieuhuyen2809@gmail.com>
2019-03-17 19:46:10 -0500

    TG_Changing Character's Animation and Debugging the bug when applying new character's animation

--- Source/TestingGround/Public/PatrolRoute.h
+++ Source/TestingGround/Public/PatrolRoute.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0cac95d9cbdc80560ad78ef0641beac1b42707aa69fa98b2da608bbc1491ecdf
-size 566
+oid sha256:8d45d45c66def7a62ff3810b05ac68bb89832021d1573907f21aa87cf4fb8e49
+size 562
